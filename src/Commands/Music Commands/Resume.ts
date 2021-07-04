b5e625aad99e703bcad0f5c54fda1bf8587dbad8
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-07-04 14:20:30 +0100

    Resume command, vc switch handling for player

---
+++ src/Commands/Music Commands/Resume.ts
@@ -0,0 +1,28 @@
+import Command from "../../Interfaces/Command";
+
+export default class ResumeCommand extends Command {
+    constructor(client) {
+        super(client, 'resume', {
+            aliases: ["continue"],
+            description: "Resume the music.",
+            category: "Music",
+        });
+    }
+    async run(message, args) {
+
+        const player = this.client.manager.players.get(message.channel.guild.id);
+        const voiceState = message.member.voiceState;
+
+        const me = message.guild.members.get(this.client.user.id);
+        if(!me.voiceState || !me.voiceState.channelID) return message.channel.createMessage({ content: `I am not connected to a Voice Channnel!`, messageReference: { messageID: message.id }});
+        if(!voiceState || voiceState.channelID !== me.voiceState.channelID) return message.channel.createMessage({ content: `You must be in the same Voice Channel as me to use this Command!`, messageReference: { messageID: message.id }});
+        if(!player || !player.paused) return message.channel.createMessage({ content: `The player doesn't seem to be paused.`, messageReference: { messageID: message.id }});
+        player.pause(false);
+        let embed = new this.client.embed()
+            .setDescription(`${message.author.mention} ▶ resumed the music!`)
+            .setColor('#ffffff');
+        message.channel.createMessage({ embed: embed, messageReference: { messageID: message.id }});
+
+     }
+
+    }
