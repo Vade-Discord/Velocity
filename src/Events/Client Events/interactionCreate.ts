e35899cf52099c69e787bdfbdbb8784d80ff5ac1
Canine8YT <elijahchristopherjohnson@gmail.com>
2021-07-18 23:31:16 -0500

    Added other interaction functionality back

--- src/Events/Client Events/interactionCreate.ts
+++ src/Events/Client Events/interactionCreate.ts
@@ -13,12 +13,36 @@ import { Interaction } from 'eris';
               await interaction.acknowledge();
               if(!interaction.data) return;
 
-              const cmd = interaction.data.custom_id.split("#")[0];
+              const com = interaction.data.custom_id.split("#")[0];
 
-              if(cmd == 'pagination') {
+              if(com == 'pagination') {
                   const pages = await this.client.Pagination.get(interaction.message.id)
                   // @ts-ignore
                   pages.run(interaction)
+              }
+              const { member } = interaction;
+              const split = interaction.data.custom_id;
+              const cmd = split.split("#")[0];
+              const id = split.split("#")[2];
+              const extra = split.split("#").length >= 3 ? split.split("#")[3] : null;
+              if(cmd === 'paginate') return;
+              // @ts-ignore
+              switch(interaction.data.component_type) {
+                  case 3: {
+                      console.log(`Selection menu`);
+                      const command = this.client.commands.get(cmd);
+                      if(!command || !command.runInteraction) return;
+                      await command.runInteraction(interaction, member, id, extra);
+                      break;
+                  }
+                  case 2: {
+                      const command = this.client.commands.get(cmd);
+                      if(!command || !command.runInteraction) return;
+                      await command.runInteraction(interaction, member, id, extra);
+                      break;
+                  }
+                  default:
+                      return console.log(`Unknown interaction type.`);
               }
 
           };
