8f21dfcd709cd36827b412ff8d1ffd3440a1c167
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-07-21 19:38:34 +0100

    Redis.ts

---
+++ src/Interfaces/Redis.ts
@@ -0,0 +1,23 @@
+import { createClient, RedisClient } from "redis";
+import { redis } from "../config.json";
+
+const redisConnect = async (): Promise<RedisClient> => {
+    return await new Promise((resolve, reject) => {
+        const client = createClient({
+            url: redis.redisPath,
+        });
+
+        client.on("error", (err) => {
+            console.error(`Redis error: `, err);
+            client.quit();
+            reject(err);
+        });
+
+        client.on("ready", () => {
+            resolve(client);
+            console.log(`Redis has connected`);
+        });
+    });
+};
+
+export default redisConnect;
