b2296d9c13917a5009b2b3e5edd73585f079cd58
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-07-22 18:19:40 +0100

    Rock paper scissors (streaks)

--- src/Commands/Fun Commands/Rockpaperscissors.ts
+++ src/Commands/Fun Commands/Rockpaperscissors.ts
@@ -46,6 +46,37 @@ export default class RpsCommand extends Command {
          const me = choices[Math.floor(Math.random() * choices.length)];
          const embed = new this.client.embed()
              .setTimestamp()
+
+         const streak = this.client.redis.get(`rpsstreak.${interaction.member.id}`);
+
+        async function handleStreak(gameStatus) {
+            switch(gameStatus) {
+                case "won": {
+                if(streak) {
+                    await this.client.redis.set(`rpsstreak.${interaction.member.id}`, streak + 1, 'EX', 86400);
+                    return streak + 1;
+                } else {
+                    await this.client.redis.set(`rpsstreak.${interaction.member.id}`, 1, 'EX', 86400);
+                    return 1;
+                    }
+                }
+                case "loss": {
+                    if(streak) {
+                        await streak.delete();
+                        return 0;
+                    } else {
+                        return 0;
+                    }
+                }
+                default:
+                    if(streak) {
+                        return streak;
+                    } else {
+                        return 'No streak.'
+                    }
+            }
+         }
+
          if (
              (me === "rock" && args[0] === "scissors") ||
              (me === "paper" && args[0] === "rock") ||
@@ -53,20 +84,28 @@ export default class RpsCommand extends Command {
          ) {
              embed.setAuthor(`${interaction.member.user.username}, you lost!`, this.client.user.avatarURL)
              embed.setColor('#F00000')
+             let streak = await handleStreak('loss');
+             embed.setFooter(`Streak status: ${streak}`, this.client.user.avatarURL)
              interaction.message.edit({ content: `Game completed!`, components: []});
             return interaction.createMessage({ embeds: [embed] });
          } else if (me === args[0]) {
              embed.setAuthor(`${interaction.member.user.username}, we tied!`, this.client.user.avatarURL)
              embed.setColor('YELLOW')
 
+             let streak = await handleStreak('tie');
+             embed.setFooter(`Streak status: ${streak}`, this.client.user.avatarURL)
              interaction.message.edit({ content: `Game completed!`, components: []});
              return interaction.createMessage({ embeds: [embed] });
          } else {
              embed.setAuthor(`${interaction.member.user.username}, you won!`, this.client.user.avatarURL)
              embed.setColor(this.client.constants.colours.green)
+             let streak = await handleStreak('won');
+             embed.setFooter(`Streak status: ${streak}`, this.client.user.avatarURL)
              interaction.message.edit({ content: `Game completed!`, components: []});
              return interaction.createMessage({ embeds: [embed] });
          }
+
+
 
      }
 
