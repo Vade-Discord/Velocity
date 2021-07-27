726a6f03a593b45f36eb83d3732035c77d82191b
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-07-27 18:09:14 +0100

    Monster.ts

---
+++ src/Commands/Image Commands/Monster.ts
@@ -0,0 +1,22 @@
+import Command from "../../Interfaces/Command";
+import { Bed } from 'discord-image-generation';
+
+export default class MonsterCommand extends Command {
+    constructor(client) {
+        super(client, 'monster', {
+            aliases: ["bed"],
+            description: "Funny bed meme.",
+            category: "Images",
+        });
+    }
+    async run(message, args) {
+
+        const member = await this.client.utils.getMember(message, args[0]);
+        if(!member) return;
+
+        const image = await new Bed().getImage(message.author.dynamicAvatarURL('png', 512), member.user.dynamicAvatarURL('png', 512));
+        message.channel.createMessage({}, { file: image, name: `bed.png`});
+
+    }
+
+}
