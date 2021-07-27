e1123fa932860b5e2cb2c53178bffdc167106bb8
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-07-27 17:41:55 +0100

    Invert.ts

---
+++ src/Commands/Image Commands/Invert.ts
@@ -0,0 +1,21 @@
+import Command from "../../Interfaces/Command";
+import { Invert } from 'discord-image-generation';
+
+export default class InvertCommand extends Command {
+    constructor(client) {
+        super(client, 'invert', {
+            description: "Apply an inverted effect to a users avatar.",
+            category: "Images",
+        });
+    }
+    async run(message, args) {
+
+        const foundmember = await this.client.utils.getMember(message, args[0], false);
+        const member = foundmember ? foundmember : message.member;
+        const avatar = member?.user.dynamicAvatarURL(`png`, 512);
+        const image = await new Invert().getImage(avatar);
+        message.channel.createMessage({}, { file: image, name: `inverted.png`});
+
+    }
+
+}
