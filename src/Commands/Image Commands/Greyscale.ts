52dd899db8a7ed3539476902b1a1daa6088d10fc
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-07-27 17:38:27 +0100

    Greyscale.ts

---
+++ src/Commands/Image Commands/Greyscale.ts
@@ -0,0 +1,22 @@
+import Command from "../../Interfaces/Command";
+import { Greyscale } from 'discord-image-generation';
+
+export default class GreyscaleCommand extends Command {
+    constructor(client) {
+        super(client, 'greyscale', {
+            aliases: ["gscale"],
+            description: "Apply a greyscale filter to a users avatar.",
+            category: "Images",
+        });
+    }
+    async run(message, args) {
+
+        const foundmember = await this.client.utils.getMember(message, args[0], false);
+        const member = foundmember ? foundmember : message.member;
+        const avatar = member?.user.dynamicAvatarURL(`png`, 512);
+        const image = await new Greyscale().getImage(avatar);
+        message.channel.createMessage({}, { file: image, name: `greyscale.png`});
+
+     }
+
+    }
