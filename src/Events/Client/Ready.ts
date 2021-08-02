c6097fd0bff3a34b565969a71e75e91355f18f62
Catboy <elijahchristopherjohnson@gmail.com>
2021-08-02 16:27:06 -0500

    Use bulkEditCommands

--- src/Events/Client/Ready.ts
+++ src/Events/Client/Ready.ts
@@ -1,29 +1,32 @@
-  import { Event } from '../../Interfaces/Event';
-  import mongo from '../../Interfaces/Database';
+import { Event } from '../../Interfaces/Event';
+import mongo from '../../Interfaces/Database';
 
-      export default class ReadyEvent extends Event {
-          constructor(client) {
-              super(client, "ready", {
-                 once: true,
-              });
-          }
+export default class ReadyEvent extends Event {
+    constructor(client) {
+        super(client, "ready", {
+            once: true,
+        });
+    }
 
-          async run() {
-              await mongo();
-              console.log(`${this.client.user.username}#${this.client.user.discriminator} has successfully logged in!`);
+    async run() {
+        await mongo();
+        console.log(`${this.client.user.username}#${this.client.user.discriminator} has successfully logged in!`);
 
-          const guild = this.client.guilds.get("857895083839324190");
+        const guild = this.client.guilds.get("857895083839324190");
 
-              this.client.editStatus('online', { name: "Vade Rewrite", type: 5, url: "https://vade-bot.com"});
+        this.client.editStatus('online', { name: "Vade Rewrite", type: 5, url: "https://vade-bot.com" });
 
-           this.client.commands.forEach( (command) => {
-               guild.createCommand({
-                   options: command.options,
-                   name: command.name,
-                   description: command.description,
-                   defaultPermission: command.devOnly,
-               });
-          })
-          }
+        const commands = []
 
-      }
+        await this.client.commands.forEach((command) => {
+            commands.push({
+                options: command.options,
+                name: command.name,
+                description: command.description,
+                defaultPermission: command.devOnly,
+            });
+        })
+        guild.bulkEditCommands(commands)
+    }
+
+}
