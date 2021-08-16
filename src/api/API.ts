5a82fa86b10922de717746d895ebfc6695b3df2a
Ethan <eth.ellis@icloud.com>
2021-08-16 17:58:09 +0100

    Add API.ts

---
+++ src/api/API.ts
@@ -0,0 +1,33 @@
+import Koa from "koa";
+import Body from "koa-body";
+import { Logger } from "@dimensional-fun/logger";
+
+import { votes } from "./routers/votes";
+import { main } from "./routers/main";
+
+import type { Bot } from "../client/Client";
+
+export class API {
+    static PORT = 2004;
+
+    readonly bot: Bot;
+
+    koa: Koa;
+
+    private log: Logger = new Logger("api");
+
+    constructor(bot: Bot) {
+        this.bot = bot;
+        this.koa = new Koa();
+        this.koa.use(Body({ json: true }));
+    }
+
+    start() {
+        this.koa.use(votes(this).middleware());
+        this.koa.use(main().middleware());
+
+        this.koa.listen(API.PORT, () =>
+            this.log.info(`Now listening on port ${API.PORT}`)
+        );
+    }
+}
