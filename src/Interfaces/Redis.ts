dba5bc3322d7001ab1dc91dc00514a460d8cf01b
V1rtuaI <eth.ellis@icloud.com>
2021-08-27 21:24:13 +0100

    Update cache logging.

--- src/Interfaces/Redis.ts
+++ src/Interfaces/Redis.ts
@@ -1,19 +1,21 @@
 import Redis from "ioredis";
 import { redis } from "../config.json";
+import { Logger } from "@dimensional-fun/logger";
 
+const logger: Logger = new Logger("cache");
 const redisConnect = async (c): Promise<Redis> => {
     return new Promise((resolve, reject) => {
         const client = new Redis(redis.port, redis.redisPath);
 
         client.on("error", (err) => {
-            console.error(`Redis error: `, err);
+            logger.error(`Redis encountered an error: `, err);
             client.quit();
             reject(err);
         });
 
         client.on("ready", () => {
             resolve(client);
-            console.log(`Redis has connected`);
+            logger.info(`Redis has successfully connected.`);
         });
 
     });
