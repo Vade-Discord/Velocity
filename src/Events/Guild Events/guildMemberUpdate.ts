51345fef609b2c7cbdf7304725a659ec453a0a8b
V1rtuaI <eth.ellis@icloud.com>
2021-08-30 19:33:20 +0100

    Fix "cannot read property nick of null"

--- src/Events/Guild Events/guildMemberUpdate.ts
+++ src/Events/Guild Events/guildMemberUpdate.ts
@@ -18,10 +18,10 @@
                   .setThumbnail(member.user.avatarURL)
                   .setTimestamp()
                   .setFooter(`Vade Logging System`, this.client.user.avatarURL)
-              if(!oldMember.nick && member.nick || oldMember.nick !== member.nick) {
+              if(!oldMember?.nick && member?.nick || oldMember?.nick !== member?.nick) {
                   logEmbed
                       .setTitle(`Nickname Updated`)
-                      .setDescription(`**From:** ${oldMember.nick ?? 'No Nickname.'}\n**To:** ${member.nick ?? 'No Nickname.'}`)
+                      .setDescription(`**From:** ${oldMember?.nick ?? 'No Nickname.'}\n**To:** ${member?.nick ?? 'No Nickname.'}`)
 
                   logChannel.createMessage({ embeds: [logEmbed] });
 
