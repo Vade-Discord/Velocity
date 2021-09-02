b633286895fb07c3ffcacad1a9ee749df05068cf
V1rtuaI <eth.ellis@icloud.com>
2021-09-02 22:33:51 +0100

    Update guildMemberUpdate.ts

--- src/Events/Guild Events/guildMemberUpdate.ts
+++ src/Events/Guild Events/guildMemberUpdate.ts
@@ -22,7 +22,7 @@
                   .setThumbnail(member.user.avatarURL)
                   .setTimestamp()
                   .setFooter(`Vade Logging System`, this.client.user.avatarURL)
-              if(!oldMember.nick && member?.nick || oldMember?.nick !== member?.nick || oldMember?.nick && !member.nick) {
+              if(!oldMember?.nick && member?.nick || oldMember?.nick !== member?.nick || oldMember?.nick && !member.nick) {
                   logEmbed
                       .setTitle(`Nickname Updated`)
                       .setDescription(`**From:** ${oldMember?.nick ?? 'No Nickname.'}\n**To:** ${member?.nick ?? 'No Nickname.'}`)
