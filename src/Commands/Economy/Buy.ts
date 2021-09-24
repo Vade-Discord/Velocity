0b59ff500c8b320a8c54e9bc213ed21dc6009b73
V1rtuaI <eth.ellis@icloud.com>
2021-09-24 18:50:54 +0100

    Update Buy.ts

--- src/Commands/Economy/Buy.ts
+++ src/Commands/Economy/Buy.ts
@@ -63,7 +63,7 @@ export default class BuyCommand extends Command {
             });
         }
 const nf = new Intl.NumberFormat();
-        interaction.createFollowup(`You have successfully purchased **${amount} ${located.name}${amount > 1 ? 's' : ''}** for a total of **${nf.format(itemPrice)}**.`);
+        interaction.createFollowup(`You have successfully purchased **${amount} ${located.name}${amount > 1 ? 's' : ''}** for a total of **$${nf.format(itemPrice)}**.`);
 
 
     }
