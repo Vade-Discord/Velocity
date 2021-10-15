c5eb20488e1914c141ffd9e2dbcabc5597ac02d7
V1rtuaI <eth.ellis@icloud.com>
2021-10-15 12:20:09 +0100

    Add Autorole.ts

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -29,7 +29,6 @@ export class Bot extends Eris.Client {
   public config: typeof Config;
   public owners: string[] = ["473858248353513472"];
   public utils: Util = new Util(this);
-  public invites = new Map();
   public constants: typeof Constants = Constants;
   public embed: typeof MessageEmbed | typeof Eris.RichEmbed = MessageEmbed;
   public manager = undefined;
@@ -41,8 +40,7 @@ export class Bot extends Eris.Client {
         "guildMessages",
         "guildVoiceStates",
         "directMessages",
-        "guildMembers",
-        "guildInvites"
+        "guildMembers"
       ],
         seedVoiceConnections: true,
         restMode: true,
