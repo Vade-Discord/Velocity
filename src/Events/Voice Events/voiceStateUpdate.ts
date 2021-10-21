2706e65a9f01cc06933f2b975c2167ed0426f22a
V1rtuaI <eth.ellis@icloud.com>
2021-10-21 14:42:41 +0100

    Push voiceStateUpdate.ts

--- src/Events/Voice Events/voiceStateUpdate.ts
+++ src/Events/Voice Events/voiceStateUpdate.ts
@@ -9,6 +9,24 @@
 
           async run(member, oldState, newState) {
 
+              const tag = `${member.username}#${member.discriminator}`;
+              const logging = (await this.client.utils.loggingChannel(member.guild, 'voice'));
+              if(!logging) {
+                  return;
+              }
+              const embed = new this.client.embed()
+                  .setAuthor(`${tag}`, member.avatarURL)
+                  .setFooter(`Vade Logging System`, this.client.user.avatarURL)
+
+              if(!oldState?.muted && newState?.muted|| oldState?.muted && !newState?.muted) {
+                  const mutedOrUnmuted = !(oldState?.muted && !newState?.muted);
+                     embed
+                      .setTitle(`${mutedOrUnmuted ? `🔇 Server-Muted` : '🔊 Server-Unmuted'}`)
+                      .setColor(mutedOrUnmuted ? "#00C09A" : "#F00000")
+                      .setTimestamp()
+
+                  logging ? logging.createMessage({ embeds: [embed] }) : null;
+              }
 
           }
 
