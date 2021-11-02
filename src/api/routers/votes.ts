d1f57c82ea709382c888182d48dfbb7b5c1fd684
V1rtuaI <eth.ellis@icloud.com>
2021-11-02 11:38:28 +0000

    Push votes.ts

--- src/api/routers/votes.ts
+++ src/api/routers/votes.ts
@@ -37,6 +37,25 @@ export function votes(api: API): Router {
             return;
         }
       await api.bot.redis.set(`votes.top.gg.${body.user}`, true, 'EX', 43200);
+        try {
+
+
+            console.log(`User with ID ${body.user} voted for the bot!`);
+            const user = (await api.bot.getRESTUser(body.user));
+            const logChannelID = api.bot.config.PRIVATE.vote_log;
+            const channel = (await api.bot.getRESTChannel(logChannelID));
+            if (channel?.type === 0) {
+                const embed = new api.bot.embed()
+                    .setDescription(`${user.username}#${user.discriminator} (\`${body.user}\`) has voted for **Velocity**! Vote for us **[HERE](https://top.gg/bot/850723996513075200/vote)**!`)
+                    .setColor(api.bot.constants.colours.green)
+                    .setTimestamp()
+                    .setFooter(`Velocity | Vote Logging`)
+
+                channel ? channel.createMessage({embeds: [embed]}) : null;
+            }
+        } catch (e) {
+            api.bot.logger.error('API ERROR: ', e);
+        }
     });
 
     return router;
