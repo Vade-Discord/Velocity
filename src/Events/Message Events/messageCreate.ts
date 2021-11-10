10aaa3ae1f6186ce77f7096b76f3c43ba7ad5d73
V1rtuaI <eth.ellis@icloud.com>
2021-11-10 19:51:31 +0000

    Update messageCreate.ts

--- src/Events/Message Events/messageCreate.ts
+++ src/Events/Message Events/messageCreate.ts
@@ -54,7 +54,7 @@ Time: <t:${Math.floor((Date.now()) / 1000)}:R>`)
 
         if (guildData?.Moderation?.antiProfanity) {
             const ignore = ['ok', 'alr'];
-            const profanity = message.content.split(" ").filter((word) => word.length >= 4 && profanityArray.some((letter) => distance(letter, word) < 0.8));
+            const profanity = message.content?.split(" ").filter((word) => word.length >= 4 && profanityArray.some((letter) => distance(letter, word?.toLowerCase()) < 0.8));
             if (profanity.length) {
                 message.delete().then(() => {
                     message.channel.createMessage(`${message.author.mention}, watch your language!`).then(async (msg) => {
