09b0bddf42eaf6a447914773d995684e34c946a5
V1rtuaI <eth.ellis@icloud.com>
2021-11-30 18:22:53 +0000

    Push Economy leaderboard in Leaderboard.ts

--- src/Commands/Statistics/Leaderboard.ts
+++ src/Commands/Statistics/Leaderboard.ts
@@ -1,5 +1,6 @@
 import Command from "../../Interfaces/Command";
 import voiceSchema from "../../Schemas/User Schemas/Voice";
+import profileSchema from "../../Schemas/User Schemas/Profile";
 import { createPaginationEmbed } from "../../Classes/Pagination";
 import { Paginate } from '@the-nerd-cave/paginate';
 import humanise from 'humanize-duration';
@@ -47,8 +48,33 @@ export default class LeaderboardCommand extends Command {
             }
 
             case "economy": {
+                const nf = new Intl.NumberFormat();
 
-                interaction.createFollowup(`Test`);
+                const allSchemas = (await profileSchema.find({ }));
+                if(!allSchemas.length) {
+                    return interaction.createFollowup("Something went wrong when locating the data!");
+                }
+
+                const allProfiles = allSchemas.sort((a, b) => b.Bank - a.Bank);
+                const mapped = allProfiles.slice(0, 10).map(async (profile) => {
+                  return  `${(await this.client.getUser(profile.User)).username}#${(await this.client.getUser(profile.User)).discriminator} - **$${nf.format(profile.Bank)}**\n\n`
+                });
+                // ensure all the profiles are loaded
+                const map = await Promise.all(mapped);
+                const pages = new Paginate(map, 8).getPaginatedArray();
+                const embeds = pages.map((page, index) => {
+                        return new this.client.embed()
+                            .setTitle(`Velocity | Economy top 10`)
+                            .setDescription(
+                                page.join("\n") ??
+                                `No more to be listed on page ${index + 1}`
+                            )
+                            .setTimestamp();
+                });
+                if(embeds.length == 1) {
+                    return interaction.createFollowup({ embeds: [embeds[0]]});
+                }
+                await createPaginationEmbed(this.client, interaction, embeds, {});
                 break;
             }
 
