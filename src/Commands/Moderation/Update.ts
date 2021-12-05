0b43e0cedb2ec38140395dd38c4cb6877a8020df
V1rtuaI <eth.ellis@icloud.com>
2021-12-05 17:14:46 +0000

    Add Update.ts

---
+++ src/Commands/Moderation/Update.ts
@@ -0,0 +1,53 @@
+import Command from "../../Interfaces/Command";
+
+export default class UpdateCommand extends Command {
+    constructor(client) {
+        super(client, 'update', {
+            description: "Update a member to match the guilds configuration.",
+            category: "Moderation",
+            modCommand: true,
+            userPerms: ['manageMembers'],
+            botPerms: ['manageNicknames', 'manageRoles'],
+            contextUserMenu: true,
+            options: [
+                {
+                    type: 6,
+                    name: 'user',
+                    description: 'The user to update.',
+                    required: true
+                }
+            ]
+        });
+    }
+    async run(interaction, member, options, subOptions) {
+
+        const u = options.get("user") ?? interaction.data?.target_id;
+        const user = (await member.guild.getMember(u))!!;
+
+        const guildData = (await this.client.utils.getGuildSchema(member.guild))!!;
+        if(guildData?.nicknameFormat && user.id !== member.guild.ownerID) {
+
+            const formatted = await this.client.utils.Interpolate(guildData.nicknameFormat, {
+                username: `${member.username}`,
+                tag: `${member.username}#${member.discriminator}`,
+                id: `${member.id}`,
+                guildName: `${member.guild.name}`,
+                guildID: `${member.guild.id}`,
+                highestRole: `${this.client.utils.getHighestRole(member, member.guild)?.name ?? 'No Role'}`,
+                wallet: `$${(await this.client.utils.getProfileSchema(member.id))?.Wallet ?? 0}`,
+                bank: `$${(await this.client.utils.getProfileSchema(member.id))?.Bank ?? 0}`,
+            }).catch(() => null);
+
+            if(formatted?.length > 32) {
+                user.edit({ nick: formatted.slice(0, 31) });
+            } else {
+                formatted ? user.edit({ nick: formatted }) : null;
+            }
+
+        }
+
+        interaction.createFollowup(`Successfully updated that member!`);
+
+    }
+
+    }
