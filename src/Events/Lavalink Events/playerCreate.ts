e6d5422162e06de64bf02b5626950265a590704e
V1rtuaI <eth.ellis@icloud.com>
2022-01-25 16:15:34 +0000

    Push playerCreate.ts and playerDestroy.ts

---
+++ src/Events/Lavalink Events/playerCreate.ts
@@ -0,0 +1,35 @@
+import { Event } from '../../Interfaces/Event';
+import {NewsChannel, TextChannel} from "eris";
+
+
+export default class PlayerCreateEvent extends Event {
+    constructor(client) {
+        super(client, "playerCreate", {
+            emitter: "manager",
+        });
+    }
+
+    async run(player) {
+
+        const guild = (await this.client.getRESTGuild(player.guild));
+        if(!guild) {
+            return;
+        }
+        const logChannelID = this.client.config.PRIVATE.music_log;
+        const logChannel = (await this.client.getRESTChannel(logChannelID))
+        if(!logChannel) {
+            return;
+        }
+        const logEmbed = new this.client.embed()
+            .setTitle("Player Created")
+            .setDescription(`**User:** ${player.user.username}#${player.user.discriminator}\n**Server:** ${guild.name} (${guild.id})\n`)
+            .setColor(this.client.colors.GREEN)
+            .setFooter(`Velocity | Music Logging`)
+            .setTimestamp()
+
+        if (logChannel instanceof NewsChannel || logChannel instanceof TextChannel) {
+            logChannel.createMessage({embeds: [logEmbed]});
+        }
+    }
+
+}
