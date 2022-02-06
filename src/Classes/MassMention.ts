36017a40a4b1bd2cd4f1e257f3eb902f9b8cf359
V1rtuaI <eth.ellis@icloud.com>
2022-02-06 02:26:25 +0000

    Make MassMention.ts a class extension.

--- src/Classes/MassMention.ts
+++ src/Classes/MassMention.ts
@@ -1,18 +1,23 @@
 import { Message } from "eris";
 import { Bot } from "../Client/Client";
+import AutoMod from "./AutoMod";
 
-export default async function MassMention(client: Bot, message: Message,): Promise<Boolean> {
-    const regex = new RegExp('<@!*&*[0-9]+>')
-    const guildSchema = await client.utils.getGuildSchema(message.guildID)
-    if(message.content.match(regex)) {
-        
-        if(message.mentions.length >= guildSchema.Moderation.mentionAmount) {
-            message.delete("Mentioning to meny user!")
+export default class MassMention extends AutoMod {
 
-            return true
-        } 
-        return false
-    } else {
-        return false
+    async run(message: Message,): Promise<Boolean> {
+        const regex = new RegExp('<@!*&*[0-9]+>')
+        const guildSchema = (await this.client.utils.getGuildSchema(message.guildID))!!;
+        if(message.content.match(regex)) {
+
+            if(message.mentions.length >= guildSchema?.Moderation["mentionAmount"]) {
+                message.delete("Mentioning to many users!");
+                return true
+            }
+            return false
+        } else {
+            return false
+        }
     }
+
+
 }
