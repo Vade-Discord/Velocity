f38ecc3795298bf3fa83bf31ae294ca9f89269ed
V1rtuaI <eth.ellis@icloud.com>
2022-02-06 02:25:13 +0000

    Make AntiEmoteSpam.ts a class extension.

--- src/Classes/AntiEmoteSpam.ts
+++ src/Classes/AntiEmoteSpam.ts
@@ -1,23 +1,29 @@
 import { Message } from "eris";
 import { Bot } from "../Client/Client";
-import nodeEmoji from 'node-emoji'
-export default async function AntiEmoteSpam(client: Bot, message: Message,): Promise<Boolean> {
-    const regex = new RegExp('^(?:<a?:\w{2,32}:)?(\d{17,19})>?$', 'gi')
+import nodeEmoji from 'node-emoji';
+import AutoMod from "./AutoMod";
+export default class AntiEmoteSpam extends AutoMod {
 
-    const args = message.content.trim().split(/ +/g);
-    const guildSchema = await client.utils.getGuildSchema(message.guildID)
-    const verifyEmoji = emoji => {
-        if (emoji.includes(':')) return  /^(?:<a?:\w{2,32}:)?(\d{17,19})>?$/g.test(emoji);
-        return !!nodeEmoji.find(emoji);
+    async run(message: Message,): Promise<Boolean> {
+        const regex = new RegExp('^(?:<a?:\w{2,32}:)?(\d{17,19})>?$', 'gi')
 
-    };
-    const emojis = args.filter(verifyEmoji);        
+        const args = message.content.trim().split(/ +/g);
+        const guildSchema = (await this.client.utils.getGuildSchema(message.guildID));
+        const verifyEmoji = emoji => {
+            if (emoji.includes(':')) return  /^(?:<a?:\w{2,32}:)?(\d{17,19})>?$/g.test(emoji);
+            return !!nodeEmoji.find(emoji);
+
+        };
+        const emojis = args.filter(verifyEmoji);
         if(emojis.length >= guildSchema.Moderation.emoteAmount) {
-            message.delete("Sending to many emotes!")
+            message.delete("Sending to many emotes!");
 
             return true
         }  else {
-        return false
+            return false
+        }
+
     }
-    
+
 }
+
