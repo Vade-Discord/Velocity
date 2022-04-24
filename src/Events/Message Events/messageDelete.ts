e617ac9e628adeef003c4e55737c51aadd0fc9f1
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2022-04-24 17:26:11 +0100

    Create messageDelete.ts

---
+++ src/Events/Message Events/messageDelete.ts
@@ -0,0 +1,37 @@
+import { Event } from '../../Interfaces/Event';
+
+
+export default class MessageDeleteEvent extends Event {
+    constructor(client) {
+        super(client, "messageDelete", {
+
+        });
+    }
+
+    async run(message) {
+
+
+        const { content } = message;
+        const guild = (await this.client.getRESTGuild(message.guildID))!!;
+        const member = (await guild.getRESTMember(message.author.id));
+
+
+
+
+        let tag = `${member.user.username}#${member.user.discriminator}`
+        let embed = new this.client.embed()
+            .setAuthor(tag, member.user.avatarURL)
+            .setTitle(`📥 Message Deleted`)
+            .setDescription(`**Channel**: ${message.channel.mention}\n\n **Member:** ${member.mention} (${member.id})\n\n**Message:** ${!message.content?.length ? "No Content" : message.content}`)
+            .setFooter(`Velocity Logging System`)
+            .setColor(`#F00000`)
+            .setTimestamp();
+
+
+        message.attachments.length ? embed.setImage(message.attachments[0].url) : null;
+        const logChannel = (await this.client.utils.loggingChannel(guild, "message"));
+        logChannel?.createMessage({ embeds: [embed] });
+
+    }
+
+}
